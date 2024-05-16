dafc1a369189c937c9025df52e58a3e5accf7bf7
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-05-16 20:57:48 +0900

    Title: 비오는 날, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240516/비오는 날/rainy-day.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+class Info {
+  public: 
+    int y;
+    int m;
+    int d;
+    std::string ds;
+    std::string w;
+};
+
+int main(void) {
+  int n;
+  std::cin >> n;
+  Info info;
+  info.y = 2101;
+
+
+  for (int i = 0; i < n; i++) {
+    int y, m, d;
+    std::string ds, w;
+    char c;
+    std::cin >> y >> c >> m >> c >> d >> ds >> w;
+    if (w == "Rain") {
+      if (y < info.y) {
+        info.y = y;
+        info.m = m;
+        info.d = d;
+        info.ds = ds;
+        info.w = w;
+      }
+      else if (y == info.y) {
+        if (m < info.y) {
+          info.y = y;
+          info.m = m;
+          info.d = d;
+          info.ds = ds;
+          info.w = w;
+        }
+        else if (m == info.y) {
+          info.y = y;
+          info.m = m;
+          info.d = d;
+          info.ds = ds;
+          info.w = w;
+        }
+      }
+    }
+  }
+  std::cout << info.y << "-" << (info.m < 9 ? "0" : "") << info.m << "-" << (info.d < 9 ? "0" : "") << info.d << " " << info.ds << " " << info.w;
+  return 0;
+}
