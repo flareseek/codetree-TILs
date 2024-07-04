7ee2f712f6a8f5690ff9566010f999a4050ddfaa
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-07-04 20:24:09 +0900

    Title: 흰검 칠하기, Time: 8ms, Memory: 2MB, Status: Wrong Answer - CodeTree

--- 240704/흰검 칠하기/painting-white-black.cpp
+++ 240704/흰검 칠하기/painting-white-black.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main(void) {
-  int N, cur = 1000;
-  int white[2001] = {0};
-  int black[2001] = {0};
-  int arr[2001] = {0};
+  int N, cur = 100000;
+  int white[200001] = {0};
+  int black[200001] = {0};
+  int arr[200001] = {0};
   std::cin >> N;
   while(N--) {
     int x;
@@ -26,7 +26,7 @@ int main(void) {
   }
   
   int w=0, b=0, g=0;
-  for (int i = 0; i < 2001; i++) {
+  for (int i = 0; i < 200001; i++) {
     if (arr[i] != 0) {
       if (white[i] + black[i] >= 4) g++;
       else if (arr[i] == 1) b++;
