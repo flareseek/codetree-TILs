a1f1abe47c9f08987d3a5b6e8ed589c4428e16dc
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-07-08 22:27:54 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 7ms, Memory: 4MB, Status: Wrong Answer - CodeTree

--- 240708/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
+++ 240708/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -8,14 +8,15 @@ int main() {
         int x1, y1, x2, y2;
         std::cin >> x1 >> y1 >> x2 >> y2;
 
-        if (x1 < fx) fx = x1;
-        if (y1 < fy) fy = y1;
-        if (lx < x2) lx = x2;
-        if (ly < y2) ly = y2;
         x1 += 1000;
         y1 += 1000;
         x2 += 1000;
         y2 += 1000;
+
+        if (x1 < fx) fx = x1;
+        if (y1 < fy) fy = y1;
+        if (lx < x2) lx = x2;
+        if (ly < y2) ly = y2;
         
         for (int x = x1; x < x2; x++)
             for (int y = y1; y < y2; y++)
