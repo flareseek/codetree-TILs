3acd59524b70624a5d7fc16bcfad722a44d1961b
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-07-08 22:26:59 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 9ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 240708/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main() {
+    bool field[2001][2001] = {false};
+    int fx = 2001, fy = 2001, lx = 0, ly = 0;
+
+    for (int i = 0; i < 2; i++) {
+        int x1, y1, x2, y2;
+        std::cin >> x1 >> y1 >> x2 >> y2;
+
+        if (x1 < fx) fx = x1;
+        if (y1 < fy) fy = y1;
+        if (lx < x2) lx = x2;
+        if (ly < y2) ly = y2;
+        
+        for (int x = x1; x < x2; x++)
+            for (int y = y1; y < y2; y++)
+                field[x][y] = true;
+    }
+    int x1, y1, x2, y2;
+    std::cin >> x1 >> y1 >> x2 >> y2;
+    for (int x = x1; x < x2; x++)
+        for (int y = y1; y < y2; y++)
+            field[x][y] = false;
+    
+    int result = 0;
+    for (int x = fx; x < lx; x++)
+        for (int y = fy; y < ly; y++) 
+            if(field[x][y]) 
+                result ++;
+    
+    std::cout << result;
+    
+    return 0;
+}
