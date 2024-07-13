42a9eacc5a7564f1c5e1a094c8ef582881d36abd
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-07-13 18:06:11 +0900

    Title: 연속되는 수 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240713/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, t, result = 0, count = 1;
+    bool is = false;
+    std::cin >> N;
+    std::cin >> t;
+    if (t >= 0) is = true;
+    N--;
+    while(N--) {
+        std::cin >> t;
+        if ((is && t >= 0) || (!is && t < 0)) count ++;
+        else {
+            is = !is;
+            if (result < count) result = count;
+            count = 1;
+        }
+    }
+    std::cout << result;
+    return 0;
+}
