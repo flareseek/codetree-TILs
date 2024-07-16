d836cfe04a3372cc93fb47b5bc175f81d84c3e5e
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-07-17 00:00:12 +0900

    Title: 만나는 그 순간, Time: 9ms, Memory: 7MB, Status: Wrong Answer - CodeTree

---
+++ 240717/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+int main() {
+    int N, M, last;
+    int A[1000001] = {0};
+    int B[1000001] = {0};
+    A[0] = 0;
+    B[0] = 0;
+    std::cin >> N >> M;
+
+    int index = 1;
+    for (int i = 0; i < N; i++) {
+        char d;
+        int t;
+        std::cin >> d >> t;
+        while(t--) {
+            if (d == 'R')
+                A[index] = A[index-1] + 1;
+            else 
+                A[index] = A[index-1] - 1;
+            index ++;
+        }
+    }
+    last = index;
+    index = 1;
+    for (int i = 0; i < M; i++) {
+        char d;
+        int t;
+        std::cin >> d >> t;
+        while(t--) {
+            if (d == 'R')
+                B[index] = B[index-1] + 1;
+            else 
+                B[index] = B[index-1] - 1;
+            index ++;
+        }
+    }
+    for (int i = 1; i < 1000001; i++) {
+        if (A[i] == B[i]) {
+            if (last <= index) std::cout << -1;
+            else std::cout << i;
+            return 0;
+        }
+    }
+    return 0;
+}
