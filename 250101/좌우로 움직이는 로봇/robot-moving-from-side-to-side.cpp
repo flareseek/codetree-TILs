eddc1824a0b55ff23e0330f42e285d1f3500cc7e
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2025-01-01 23:28:30 +0900

    Title: 좌우로 움직이는 로봇, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250101/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int A, B;
+    vector<int> aVector, bVector;
+    aVector.push_back(0);
+    bVector.push_back(0);
+    cin >> A >> B;
+    int t1;
+    char t2;
+    while(A--) {
+        cin >> t1 >> t2;
+        while(t1--) {
+            aVector.push_back(aVector.back() + (t2 == 'R' ? 1 : -1));
+        }
+    }
+    while(B--) {
+        cin >> t1 >> t2;
+        while(t1--) {
+            bVector.push_back(bVector.back() + (t2 == 'R' ? 1: -1));
+        }   
+    }
+
+    int aIndex = 0;
+    int bIndex = 0;
+    int aSize = aVector.size();
+    int bSize = bVector.size();
+    int count = 0;
+    int i = 1;
+    while(true) {
+        aIndex = i < aSize ? i : (aSize - 1);
+        int aPrev = aIndex == 0 ? aIndex : (aIndex - 1);
+        bIndex = i < bSize ? i : (bSize - 1);
+        int bPrev = bIndex == 0 ? bIndex : (bIndex - 1);
+       
+        if (aVector[aPrev] != bVector[bPrev] && aVector[aIndex] == bVector[bIndex]) count ++;
+        if (aIndex == (aSize-1) && bIndex == (bSize-1)) break;
+        i++;
+    }
+    
+    cout << count;
+
+    return 0;
+}
